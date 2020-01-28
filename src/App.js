808f4a8b508fe7ee62fe629458fb421cbf69fdee
pelzolga123 <pelzolga123@gmail.com>
2020-01-28 12:03:00 +0200

    Added BooksList and BooksForm to App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import './App.css';
+import BooksList from './components/BooksList';
+import BooksForm from './components/BooksForm';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BooksList />
+      <BooksForm />
     </div>
   );
 }
