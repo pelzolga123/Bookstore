07781de8b52fe72870674bbc329358c433a1fa35
tundeiness <tundeiness@yahoo.co.uk>
2020-01-28 11:19:34 +0100

    create Booklist

---
+++ src/component/booklist.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BookList = () => (
+  // design a table.
+  <div className="row">
+    <div className="column" />
+    <div className="column" />
+  </div>
+);
+
+export default BookList;
