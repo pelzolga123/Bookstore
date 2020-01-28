8e5c1035bafbc7c01a794f9c70d123b4c2bf55d4
pelzolga123 <pelzolga123@gmail.com>
2020-01-28 16:53:54 +0200

    Finished App.js, added default books

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './component/App';
+import rootReducer from './reducers';
+
+const BOOKS = {
+  books: [
+    {
+      id: '0',
+      title: 'Test One',
+      category: 'Sci-Fi',
+    },
+    {
+      id: '1',
+      title: 'Test Two',
+      category: 'Fantasy',
+    },
+  ],
+
+};
+
+const store = createStore(rootReducer, BOOKS);
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
