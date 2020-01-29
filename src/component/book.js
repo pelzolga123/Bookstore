be07da82a67f8f24b7e7ce7b026ce0d2c9650da3
pelzolga123 <pelzolga123@gmail.com>
2020-01-29 13:51:26 +0200

    Added handleRemoveBook

--- src/component/book.js
+++ src/component/book.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Book = ({ book }) => (
-  <tr>
-    <th scope="col">{book.id}</th>
-    <th scope="col">{book.title}</th>
-    <th scope="col">{book.category}</th>
-  </tr>
-);
+const Book = ({ book, removeBook }) => {
+  const handleRemoveBook = () => removeBook(book.id);
+  return (
+    <tr>
+      <th scope="col">{book.id}</th>
+      <th scope="col">{book.title}</th>
+      <th scope="col">{book.category}</th>
+      <th>
+        <button type="submit" onClick={handleRemoveBook}>Remove Book</button>
+      </th>
+    </tr>
+  );
+};
 
 Book.propTypes = {
   book: PropTypes.exact({
@@ -15,6 +21,7 @@ Book.propTypes = {
     title: PropTypes.string,
     category: PropTypes.string,
   }).isRequired,
+  removeBook: PropTypes.func.isRequired,
 };
 
 export default Book;
--- src/container/booklist.js
+++ src/container/booklist.js
@@ -1,32 +1,48 @@
+/* eslint-disable max-len */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Book from '../component/book';
+import { removeBook } from '../actions/index';
 
-const BookList = props => {
-  const { books } = props;
-  return (
-    <div className="main">
-      <h1>Bookstore</h1>
-      <table className="table">
-        <thead className="thead-dark">
-          <tr>
-            <th scope="col">Id</th>
-            <th scope="col">Title</th>
-            <th scope="col">Category</th>
-          </tr>
-        </thead>
-        <tbody>{books.map(book => <Book key={book.id} book={book} />)}</tbody>
-      </table>
+class BookList extends React.Component() {
+  constructor(props) {
+    super(props);
+    this.handleBookRemove = this.handleBookRemove.bind(this);
+  }
 
-    </div>
-  );
-};
+  handleBookRemove(book) {
+    const { removeBook } = this.props;
+    removeBook(book);
+  }
+
+  render() {
+    const { books } = this.props;
+    return (
+      <div className="main">
+        <h1>Bookstore</h1>
+        <table className="table">
+          <thead className="thead-dark">
+            <tr>
+              <th scope="col">Id</th>
+              <th scope="col">Title</th>
+              <th scope="col">Category</th>
+            </tr>
+          </thead>
+          <tbody>{books.map(book => <Book handleBookRemove={() => this.handleRemoveBook(book)} key={book.id} book={book} />)}</tbody>
+        </table>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   books: state.books,
 });
 
+const mapDispatchToProps = dispatch => ({
+  removeBook: book => dispatch(removeBook(book)),
+});
 
 BookList.propTypes = {
   books: PropTypes.arrayOf(
@@ -36,6 +52,7 @@ BookList.propTypes = {
       Category: PropTypes.string,
     }).isRequired,
   ).isRequired,
+  removeBook: PropTypes.instanceOf(Function).isRequired,
 };
 
-export default connect(mapStateToProps)(BookList);
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
